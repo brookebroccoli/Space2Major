45578285b60ee631d7bf64d0f0eac6b8865aee8e
brookiechookie <33395020+brookiechookie@users.noreply.github.com>
2020-11-03 12:36:21 +1100

    Made MoveToObject file
    And some other changes

---
+++ TestMoveToObject.py
@@ -0,0 +1,10 @@
+# Echo client program
+import socket
+import time
+
+# Import all functions from MoveToObject
+from MoveToObject import *
+
+# Set the connection 
+HOST = "192.168.0.10"   # The remote host
+PORT = 30002            # The same port as used by the server
--- main.py
+++ main.py
@@ -2,28 +2,68 @@
 import socket
 import time
 
-HOST = "192.168.0.10" # The remote host
-PORT = 30002 # The same port as used by the server
+# Import all functions from MoveToObject 
+from MoveToObject import *
+# Or can be written as
+# import MoveToObject as MoveCommand
+# Used as MoveCommand.MoveToObject(1,1,1,0,0,0)
 
-print "Starting MoveToObject Function"
 
-def MoveToObject(x,y,z,rx,ry,rz)
+HOST = "192.168.0.10"   # The remote host
+PORT = 30002            # The same port as used by the server
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    time.sleep(0.5)
+# For compilation purposes
+x = 1
+y = 1
+z = 1
 
-    print "Robot Moves to a point above object"
-    s.send ("movej(p[x,y,z,rx,ry,rz], a=0.1, v=0.1)" + "\n")
-    time.sleep(10)
+# Need to find these positions in the Lab. These are set positions
+HomePosition        = [x, y, z]
+PhotoPosition       = [x, y, z]
+ContainerPosition   = [x, y, z]
 
-    print "Set output 1 high"
-    s.send ("set_digital_out(1,True)" + "\n")
-    time.sleep(0.1)
+# Assign home position (we will have to do this in the lab - get base position
+# - whatever you desire to be base)
+# NB: can just make this the photo positon
 
-    print "Program finish"
-    time.sleep(1)
-    data = s.recv(1024)
-    s.close()
-    print ("Received", repr(data))
-    print "Status data received from robot"
+RunLoop = 1
+count   = 1
+
+while True:
+    # First find the photo positon coordinates - manually move
+    # Now assign this as the photo position as a POSE (coords + angles)
+    # Move the UR5 there - using MoveToObject
+    #MoveToObject(PhotoPosition,0,0,0)
+
+
+    ### Rock finding(rockinvalid[]) (Elle)
+    ### Feed in matrix of invalid rock coordianates [x,y,z]
+    RockCoordinates     = [x, y, z]
+
+
+    ### Take the coordinates given by rock finding
+    # Move arm to rock coordinates
+    #MoveToObject(RockCoordinates,0,0,0)
+
+
+    ### Linear movement down to get rock
+
+
+    # Determine if we can pick up rock - by trying to lift it. If the force is
+    # bigger then threshold value then leave that rock
+    # Save the location of the unliftable rock
+    # Else; rock is liftable, call Jacks function to pick up rock
+
+
+    # Go to container position
+    #MoveToObject(ContainerPosition,0,0,0)
+    ### Unclamp gripper to drop rock in container
+
+    if count == 9:
+        print "Exiting while loop"
+        break
+
+    print "Count is up to ", count
+    count += 1
+
+print "Main has compiled"
