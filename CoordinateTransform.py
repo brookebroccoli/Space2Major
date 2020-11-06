407b7630b2edd6e05d70a89250c85da50e366219
jayden <jayden.schofield@gmail.com>
2020-11-06 17:23:49 +1100

    added wrist adding

--- CoordinateTransform.py
+++ CoordinateTransform.py
@@ -1,13 +1,16 @@
 #Returns the new pose from a camera determined XYZ position
 #must pass in xyz of object from camera and position this object was taken from
-def CameraXYZToPose(CameraXYZ, FromPose)
+def CameraXYZToPose(CameraXYZ, FromPose, WristPose)
     #fixed vector length from TCP to lens
-    TcpToCamera = np.array([0,0,0]) #must measure this
+    TcpToCamera = np.array([0,0,0]) #must measure this manually
 
     #declare numpy arrys for vector addition
     CameraToObject = np.array(CameraXYZ)
-    BaseToTCP = np.array(FromPose)
+    BaseToTCP = np.array(FromPose[0:3])
 
     #vector addition, note still must include vector diff from TCP to lens
     NewPose = BaseToPhotoTCP + TcpToCamera + CameraToObject
+    #add the wrist position elements
+    NewPose = np.concatenate((NewPose, WristPose), axis=0)
+
     return NewPose
